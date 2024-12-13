1edfc819565d4e9d2e2d8d77c9b320e8b184d306
YoDaneri00 <121862972+YoDaneri00@users.noreply.github.com>
2024-12-13 12:44:57 +0100

    Update Lenguaje_SQL.sql

--- Lenguaje_SQL.sql
+++ Lenguaje_SQL.sql
@@ -16,3 +16,19 @@ SELECT * FROM Base_01.Usuarios;
 
 -- 19. LLENAR UNA TABLA CON DATOS
 
+-- Creamos la misma Tabla que anteriormente en Base_02 para
+
+USE Base_02;
+CREATE TABLE Usuarios (id int(2) primary key, nombre varchar(50), apellido varchar(50), correo varchar(100));
+
+-- Sintaxis Genérica para Insertar Datos
+
+INSERT INTO <Nombre_Tabla> VALUES (<Valores>);
+
+-- Sintaxis Específica
+
+INSERT INTO Usuarios VALUES (1, 'Daniel', 'Daneri', 'daneri@gmail.com');
+INSERT INTO Usuarios VALUES (2, 'Emilio', 'Piletas', 'piletas@gmail.com');
+
+-- 20. SENTENCIA SELECT
+
