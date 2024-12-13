d742e1ce9cfde17fcf7a7d563580cdc41e148bc7
YoDaneri00 <121862972+YoDaneri00@users.noreply.github.com>
2024-12-13 12:10:17 +0100

    Update Lenguaje_SQL.sql

--- Lenguaje_SQL.sql
+++ Lenguaje_SQL.sql
@@ -2,3 +2,17 @@
 
 CREATE DATABASE <Nombre_Base_Datos>;
 
+-- 18. CREAR TABLAS
+
+CREATE TABLE <Nombre_Tabla> (<Columnas>);
+
+-- -- Ejemplo
+
+CREATE TABLE Usuarios (id int(2) primary key, nombre varchar(50), apellido varchar(50), correo varchar(100));
+
+-- -- La Visualizamos (Tabla Vacía en este momento)
+
+SELECT * FROM Base_01.Usuarios;
+
+-- 19. LLENAR UNA TABLA CON DATOS
+
