68dff14797cb21f3faaae3e6c6153af3e83fe8f2
YoDaneri00 <121862972+YoDaneri00@users.noreply.github.com>
2024-12-17 09:26:41 +0100

    Create 05. Ejercicios.sql

---
+++ 05. Ejercicios.sql
@@ -0,0 +1,24 @@
+-- EJERCICIO 1
+
+CREATE TABLE Libros 
+  (
+  libro_id INT PRIMARY KEY, 
+  titulo VARCHAR(255) NOT NULL, 
+  autor VARCHAR(100) NOT NULL, 
+  fecha_publicacion DATE, 
+  genero VARCHAR(50)
+  );
+
+-- EJERCICIO 2
+
+CREATE TABLE Ventas 
+  (
+  venta_id INT PRIMARY KEY, 
+  fecha_venta DATETIME, 
+  monto_total DECIMAL(10,2), 
+  metodo_pago VARCHAR(20), 
+  cliente_id INT
+  );
+
+-- EJERCICIO 3
+
