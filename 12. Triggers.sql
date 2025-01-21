6c179524e242d1171c2816d49084ffb3b43cf104
YoDaneri00 <121862972+YoDaneri00@users.noreply.github.com>
2025-01-21 10:27:06 +0100

    Update 12. Triggers.sql

--- 12. Triggers.sql
+++ 12. Triggers.sql
@@ -33,3 +33,39 @@ DELIMITER ;
 -- Usamos Trigger
 
 INSERT INTO usuarios (nombre, correo) VALUES ('Daniel Daneri', 'danieldaneri@gmail.com')
+
+-- 67. Triggers II (Disparadores II)
+
+-- Creamos Tabla
+
+CREATE TABLE productos (
+	id_producto INT AUTO_INCREMENT PRIMARY KEY,
+	nombre_producto VARCHAR(100) NOT NULL,
+	precio DECIMAL(10, 2) NOT NULL
+);
+
+-- Creamos Trigger
+
+DELIMITER //
+	
+CREATE TRIGGER validar_precio
+	BEFORE INSERT ON productos
+	FOR EACH ROW
+	BEGIN
+		-- Validar Precio
+		-- Obtenemos Parámetro Insertado
+		IF NEW.precio <= 0 THEN
+		SIGNAL SQLSTATE ‘4500’ SET MESSAGE_TEXT = ‘Precio incorrecto’;
+		END IF;
+	END
+//
+
+DELIMITER ;
+
+-- Testamos Trigger (Sin Error)
+
+INSERT INTO productos (nombre_producto, precio) VALUES (‘Portátil’, 1200.55);
+
+-- Testamos Trigger (Con Error)
+
+INSERT INTO productos (nombre_producto, precio) VALUES (‘Portátil’, -1200.55);
