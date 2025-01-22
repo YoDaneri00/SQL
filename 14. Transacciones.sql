caebaa65ed6443d391dba8ff85e8e92f85d8e80f
YoDaneri00 <121862972+YoDaneri00@users.noreply.github.com>
2025-01-22 13:50:29 +0100

    Update 14. Transacciones.sql

--- 14. Transacciones.sql
+++ 14. Transacciones.sql
@@ -1,6 +1,37 @@
 -- 72. Transacciones (Práctica)
 
+-- Sin Error
 
+	BEGIN;
+
+	-- Restar Saldo Juan (200 $)
+	UPDATE cuentas SET saldo = saldo – 200.00 WHERE cuenta_id = 1;
+
+	-- Hacer Transacción
+	UPDATE cuentas SET saldo = saldo + 200.00 WHERE cuenta_id = 2;
+
+	-- Confirmar Transacción
+	COMMIT;
+
+	ROLLBACK;
+
+-- Con Error
+
+	BEGIN;
+
+	-- Restar Saldo Juan (200 $)
+	UPDATE cuentas SET saldo = saldo – 200.00 WHERE cuenta_id = 1;
+
+  -- Error
+  INSERT INTO cuentas (cuenta_id, titular, saldo) VALUES (1, ‘Error en la transacción’, 100);
+
+	-- Hacer Transacción
+	UPDATE cuentas SET saldo = saldo + 200.00 WHERE cuenta_id = 2;
+
+	-- Confirmar Transacción
+	COMMIT;
+
+	ROLLBACK;
 
 -- 73. Transacciones (Práctica II)
 
