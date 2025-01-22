3afa10caf6c397a1d18914cf4f4133d675549312
YoDaneri00 <121862972+YoDaneri00@users.noreply.github.com>
2025-01-22 11:04:57 +0100

    Update 13. Vistas.sql

--- 13. Vistas.sql
+++ 13. Vistas.sql
@@ -4,12 +4,12 @@ DELIMITER //
 
 CREATE VIEW VentasTotales AS 
 	SELECT 
-		p.Nombre AS Nombre_Producto, 
-    SUM(v.Cantidad) AS Total_Cantidad,
-    SUM(v.Cantidad * p.Precio) AS Ingreso_Total
+	p.Nombre AS Nombre_Producto, 
+   	SUM(v.Cantidad) AS Total_Cantidad,
+    	SUM(v.Cantidad * p.Precio) AS Ingreso_Total
 	FROM productos p
-		JOIN ventas v ON p.id_Productos = v.id_Productos
-  GROUP BY p.Nombre;
+	JOIN ventas v ON p.id_Productos = v.id_Productos
+ 	GROUP BY p.Nombre;
 //
 
 DELIMITER ;
