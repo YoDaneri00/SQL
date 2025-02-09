f35dcbfebd9dbbd1fb839fdcd4aefb4ad61f25d8
YoDaneri00 <121862972+YoDaneri00@users.noreply.github.com>
2025-02-09 12:24:23 +0100

    Update 24. Login.sql

--- 24. Login.sql
+++ 24. Login.sql
@@ -6,7 +6,7 @@
 		‘ID_Usuario’ INT NOT NULL AUTO_INCREMENT,
 		‘Usuario’ VARCHAR(45) NOT NULL,
 		‘Contraseña’ VARCHAR(45) NOT NULL,
-		‘Fecha_Registro’ VARCHAR(45) NULL DEFAULT ‘CURRENT_TIMESTAMP()’,
+		‘Fecha_Registro’ VARCHAR(45) NULL DEFAULT CURRENT_TIMESTAMP(),
 	PRIMARY KEY (‘ID_Usuario’));
 
 -- Insertar Datos
@@ -19,5 +19,10 @@
 	INSERT INTO Usuarios (Usuario, Contraseña) VALUES (‘Clotilde’, ‘Clotilde00’);
 	INSERT INTO Usuarios (Usuario, Contraseña) VALUES (‘Antunez’, ‘Antunez00’);
 
--- Consultar Datos
+-- Iniciamos Auto-Incrementable
 
+	ALTER TABLE Usuarios AUTO_INCREMENT = 1;
+
+-- Consultamos Datos
+
+	SELECT * FROM Usuarios;
