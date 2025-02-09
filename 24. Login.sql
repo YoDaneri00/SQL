52f3b04c25484bad227c3862b740b23d36871134
YoDaneri00 <121862972+YoDaneri00@users.noreply.github.com>
2025-02-09 11:50:57 +0100

    Update 24. Login.sql

--- 24. Login.sql
+++ 24. Login.sql
@@ -9,4 +9,15 @@
 		‘Fecha_Registro’ VARCHAR(45) NULL DEFAULT ‘CURRENT_TIMESTAMP()’,
 	PRIMARY KEY (‘ID_Usuario’));
 
--- 
+-- Insertar Datos
+
+	USE Login;
+
+	INSERT INTO Usuarios (Usuario, Contraseña) VALUES (‘YoDaneri’, ‘Android00’);
+	INSERT INTO Usuarios (Usuario, Contraseña) VALUES (‘Manuela’, ‘Manuela00’);
+	INSERT INTO Usuarios (Usuario, Contraseña) VALUES (‘Laura’, ‘Laura00’);
+	INSERT INTO Usuarios (Usuario, Contraseña) VALUES (‘Clotilde’, ‘Clotilde00’);
+	INSERT INTO Usuarios (Usuario, Contraseña) VALUES (‘Antunez’, ‘Antunez00’);
+
+-- Consultar Datos
+
